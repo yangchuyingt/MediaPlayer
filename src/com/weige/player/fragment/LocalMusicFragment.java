b20fbe1a376ad895370d02763e89f9b08bdd90ee
yangchuyin <1191378206@qq.com>
2015-05-11 18:07:12 +0800

    写了一下本地音乐的布局，功能暂时为实现

--- src/com/weige/player/fragment/LocalMusicFragment.java
+++ src/com/weige/player/fragment/LocalMusicFragment.java
@@ -1,21 +1,63 @@
 package com.weige.player.fragment;
 
 import com.weige.player.R;
+import com.weige.player.adapter.Spinneradapter;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class LocalMusicFragment extends Fragment {
+public class LocalMusicFragment extends Fragment implements OnCheckedChangeListener, OnItemClickListener {
 
-	private View localmusicView;
+	private View view;
+	private RadioGroup rg_local_music;
+	private Spinner sp_local_music;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		localmusicView = inflater.inflate(R.layout.localmusic, container, false);
-		return localmusicView;
+		view = inflater.inflate(R.layout.localmusic, container, false);
+		initview();
+		initdata();
+		return view;
 	}
+
+	private void initview() {
+		rg_local_music = (RadioGroup)view.findViewById(R.id.rg_local_music);
+		((RadioButton)rg_local_music.getChildAt(0)).setChecked(true);
+		sp_local_music = (Spinner) view.findViewById(R.id.sp_local_music);
+		
+		
+	}
+	
+	private void initdata() {
+		rg_local_music.setOnCheckedChangeListener(this);
+		sp_local_music.setOnItemClickListener(this);
+		Spinneradapter adapter =new Spinneradapter();
+		sp_local_music.setAdapter(adapter);
+		
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
